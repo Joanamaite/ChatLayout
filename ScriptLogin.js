a4d1cf42ef53c7ad4605fbb18f6b86b71b15dc5a
Joana <joanamaite0@gmail.com>
2023-08-06 19:28:16 -0300

    Ajustes de paginas

--- ScriptLogin.js
+++ ScriptLogin.js
@@ -2,9 +2,13 @@ let nomeUsuario = document.querySelector("#input");
 let botao = document.querySelector("#botao");
 
 botao.onclick = async () => {
-    let nick = nomeUsuario.value;
-    console.log(nick);
-    await axios.post("https://chat-crng.onrender.com/entrar", nick).then(result => console.log(result.data)).catch(err => console.log(err));
-    console.log(result.token);
-}
-
+  let nick = nomeUsuario.value;
+  console.log(nick);
+  try {
+    const response = await axios.post("https://2a8eaf29-2e50-4e68-9c67-e84183a2d3e9.mock.pstmn.io/entrar",{ nick });
+    console.log(response.data);
+    console.log(response.data.token); 
+  } catch (error) {
+    console.log(error);
+  }
+};
